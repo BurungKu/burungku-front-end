219eeeee34fa6ece1d449cc622626760fe17d101
threeanra <trian.radis448@gmail.com>
2025-12-02 20:20:04 +0700

    feat: hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,30 @@
 import {Github} from "lucide-react";
 import logo from "@/assets/BurungKuLogo.svg";
 import {Button} from "@/components/ui/button.tsx";
+import Container from "@/components/Container.tsx";
 
 export default function Navbar() {
 
     return (
-        <nav>
-            <div className="flex justify-between py-5">
-                <div className="flex items-center">
-                    <img src={logo} alt="Logo" className="w-14 h-14" />
-                    <span>Burung<span className="font-bold">Ku.</span></span>
-                </div>
+        <nav className="absolute w-full">
+            <Container>
+                <div className="flex justify-between py-5">
+                    <div className="flex items-center">
+                        <img src={logo} alt="Logo" className="w-14 h-14"/>
+                        <span>Burung<span className="font-bold text-theme-orange">Ku.</span></span>
+                    </div>
 
-                <div className="mt-2">
-                    <Button className="flex items-center gap-2">
-                        <Github size="18"/>
-                        Repository
-                    </Button>
+                    <div className="flex items-center gap-3">
+                        <div className="">
+                            Policy
+                        </div>
+                        <div className="h-5 w-[1px] bg-gray-200"></div>
+                        <Button className="flex items-center gap-2">
+                            <Github size="18"/>
+                        </Button>
+                    </div>
                 </div>
-            </div>
+            </Container>
         </nav>
     )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,24 @@
 import Navbar from "@/components/Navbar.tsx";
 import Container from "@/components/Container.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 export default function App() {
 
     return (
-        <div className="h-dvh">
+        <div className="relative min-h-screen w-full">
+            <Navbar/>
             <Container>
-                <Navbar/>
-
+                <div className="flex items-center justify-center h-dvh flex-col">
+                    <div className="flex flex-col items-center gap-3">
+                        <span className="text-4xl font-thin">kenali burung<span className="font-bold">Mu.</span></span>
+                        <span className="text-2xl font-thin">membantu burung langka agar tetap lestari</span>
+                    </div>
+                    <div className="flex flex-row gap-3 items-center mt-6">
+                        <Button>Analisa Gambar</Button>
+                        <div className="h-5 w-[1px] bg-gray-200"></div>
+                        <Button variant="ghost" className="border border-theme-orange text-theme-orange">Analisa Suara</Button>
+                    </div>
+                </div>
             </Container>
         </div>
     )
