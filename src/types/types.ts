4307830ee8fd536f65b53ebe03ad88ae805faa49
threeanra <trian.radis448@gmail.com>
2025-12-07 18:45:19 +0700

    feat: add result data audio type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,14 @@
 export type IucnType = "EX" | "EW" | "CR" | "EN" | "VU" | "NT" | "LC"
 
+export type PredictedAudioType = "song" | "alarm" | "flight_call" | "begging" | "mechanical" | "contact_call" | "uncertain"
+
 export type ResultData = {
     species: string
     score: number
     iucn: IucnType | null
+}
+
+export type ResultDataAudio = {
+    species: string
+    predicted_simple_type: PredictedAudioType | null
 }
